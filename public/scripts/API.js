310278bd1c71e7d0a0c77478ad9e02ea6f6b71f0
shahid <shahidhashmiraj@gmail.com>
2024-05-27 23:35:02 +0530

    Web-Authentication-APIs

---
+++ public/scripts/API.js
@@ -0,0 +1,29 @@
+const API = {
+    endpoint: "/auth/",
+    // ADD HERE ALL THE OTHER API FUNCTIONS
+    login: async (user) => {
+        return await API.makePostRequest(API.endpoint + "login", user);
+    },
+    register: async (user) => {
+        return await API.makePostRequest(API.endpoint + "register", user);
+    },
+    makePostRequest: async (url, data) => {
+        try {
+            
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            
+            return  response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+}
+
+export default API;
